d1b4f56f64ccd934addc1b72da7975a9767a48bb
Yuji Nelson <k.yuji.nelson@gmail.com>
2019-11-07 23:01:14 -0600

    added basic account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_secure_password
   has_many :assignments
   has_many :discipleship_communities
   has_many :small_groups
   has_many :user_roles
+
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
 end
