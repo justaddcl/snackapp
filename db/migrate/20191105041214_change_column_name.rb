01aa1aeca8fce2552887ce07d99c4fd2c901ba44
Yuji Nelson <k.yuji.nelson@gmail.com>
2019-11-09 23:07:29 -0600

    renamed db columns to reduce redundancy
    - removed table name from columns
    - added ID to index pages lists

--- db/migrate/20191105041214_change_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :small_groups, :user_id, :leader_id
-    rename_column :discipleship_communities, :user_id, :pastor_id
-  end
-end
