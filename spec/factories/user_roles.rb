7d52004e0c5367ac25a1ffff53f1bd0b73499ec2
Danieth <daniel.joseph.ackerman@gmail.com>
2019-11-24 23:39:57 -0600

    Fixed schema generation issues by flipping to SQL schema over ruby schema.rb (allowing for ENUMS)

--- spec/factories/user_roles.rb
+++ spec/factories/user_roles.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user_role do
-    
+    association :small_group, factory: :small_group
+    association :user, factory: :user
+
+    type { UserRole.types.values.sample }
   end
 end
