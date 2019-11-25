6ee3e17cc2376e50d17f685811c7636e07317405
Danieth <daniel.joseph.ackerman@gmail.com>
2019-11-24 23:45:22 -0600

    Ran       invoke  active_record       remove    db/migrate/20191125054522_create_event_types.rb       remove    app/models/event_type.rb       invoke    test_unit       remove      test/models/event_type_test.rb       remove      test/fixtures/event_types.yml       invoke  controller       remove    app/controllers/event_types_controller.rb       invoke    erb       remove      app/views/event_types       invoke    test_unit       remove      test/controllers/event_types_controller_test.rb       invoke    helper       remove      app/helpers/event_types_helper.rb       invoke      test_unit       invoke    assets       invoke      scss       remove        app/assets/stylesheets/event_types.scss       invoke  resource_route        route    resources :event_types

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,6 @@ class Event < ApplicationRecord
   # validates :date_is_in_the_future?
   # validates :date_is_in_the_future?, if: :new_record?
 
-
   def date_is_in_the_future?
     date < Date.today
     # errors.add(:date, "can't be in the past") if date < Date.today
