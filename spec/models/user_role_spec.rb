2a2522998587f1e57241a6571fe87665b0344aeb
Danieth <daniel.joseph.ackerman@gmail.com>
2019-11-25 00:36:01 -0600

    Removed optional dependency on user_role

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe UserRole, type: :model do
   it "should have valid factories" do
     expect(build(:user_role)).to be_valid
+    expect(build(:user_role, :with_small_group)).to be_valid
   end
 end
