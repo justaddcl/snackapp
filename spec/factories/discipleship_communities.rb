579a62a486bfb47cde6c5acc45ef0647655cfc3c
Danieth <daniel.joseph.ackerman@gmail.com>
2019-11-24 23:04:08 -0600

    Added discipleship_community factory spec

--- spec/factories/discipleship_communities.rb
+++ spec/factories/discipleship_communities.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :discipleship_community do
-    
+    association :pastor, factory: [:user, :admin]
+    night { ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"].sample }
   end
 end
