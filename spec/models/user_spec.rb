19704ecbd735400cc42282eeaf1fcd67414c0416
Yuji Nelson <k.yuji.nelson@gmail.com>
2019-11-25 22:55:35 -0600

    created user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,44 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before(:all) do
+    #user1 = build(:user)
+  end
+
   it "should have valid factories" do
     expect(build(:user)).to be_valid
     expect(build(:user, :admin)).to be_valid
+  end
+
+  it "is not valid without a first name" do
+    user = build(:user, first_name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a last name" do
+    user = build(:user, last_name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "must have a unique email" do
+    user1 = create(:user, email: 'example@site.com')
+    user2 = build(:user, email: 'example@site.com')
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    user = build(:user, password: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a password of at least 8 characters " do
+    user = build(:user, password: 'a')
+    expect(user).to_not be_valid
   end
 
 end
