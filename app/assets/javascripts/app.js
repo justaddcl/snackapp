f3d7745a8583acce0c72c7ed5f0b2a79bef15d1c
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-01-25 23:24:08 -0600

    [WIP] mobile menu
    - added: added links to the menu items
    - broken: log out only working with button_to not link_to  (any non-GET link isn't firing correctly)

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,14 @@
 const menuButton = document.querySelector('button.header__menu');
 const mobileMenu = document.querySelector('nav.nav-mobile');
+const main = document.querySelector('main.main');
 
 function toggleMobileMenu() {
   const open = mobileMenu.getAttribute('aria-hidden') === 'true' || false;
   mobileMenu.setAttribute('aria-hidden', !open);
+  document.querySelector('.header__menu-icon').classList.toggle('fa-times');
+  document.querySelector('.header__menu-icon').classList.toggle('fa-bars');
   mobileMenu.classList.toggle('nav-mobile--open');
+  main.classList.toggle('main--modal-open');
 }
 
 menuButton.addEventListener('click', toggleMobileMenu);
