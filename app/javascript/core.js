13fff50236d68b268fd4e93d6fd4d411806dbb30
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-01-26 22:56:45 -0600

    [WIP] desktop menu
    - added: desktop menu links
    - TODO: finish dropdowns
    - mobile menu is stable

--- app/javascript/core.js
+++ app/javascript/core.js
@@ -3,8 +3,6 @@ const mobileMenu = document.querySelector('nav.nav-mobile');
 const main = document.querySelector('main.main');
 
 function toggleMobileMenu() {
-  const open = mobileMenu.getAttribute('aria-hidden') === 'true' || false;
-  mobileMenu.setAttribute('aria-hidden', !open);
   document.querySelector('.header__menu-icon').classList.toggle('fa-times');
   document.querySelector('.header__menu-icon').classList.toggle('fa-bars');
   mobileMenu.classList.toggle('nav-mobile--open');
