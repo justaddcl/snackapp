9509f7e45d965bb64caffe4421ac5282ff7c521e
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-12 21:59:17 -0500

    small group
    added:
    - edit button styling
    - flex:1 to group leader photo to make it full size in firefox
    
    updated:
    - back button to be an inline semantic link
    - user.js to change innerText not innerHTML
    - user.js grabs and sets elements through the event object

--- app/javascript/users.js
+++ app/javascript/users.js
@@ -1,13 +1,13 @@
-const toggleElement = document.querySelector('.group__list-toggle');
+const toggleElement = document.querySelector('.group__list-toggle') || null;
 
-const toggleListView = () => {
-  const collapsed = toggleElement.innerHTML.includes('Show');
-  const iconElement = document.querySelector('.group__list-toggle-icon');
+const toggleListView = e => {
+  const collapsed = e.currentTarget.firstElementChild.innerText.includes('Show');
+  const iconElement = e.currentTarget.querySelector('.group__list-toggle-icon');
   const icons = [
     collapsed ? 'fa-chevron-down' : 'fa-chevron-up',
     collapsed ? 'fa-chevron-up' : 'fa-chevron-down',
   ];
-  toggleElement.innerHTML = collapsed ? 'Collapse details' : 'Show details';
+  toggleElement.firstElementChild.innerText = collapsed ? 'Collapse details' : 'Show details';
   iconElement.classList.replace(icons[0], icons[1]);
   document.querySelector('.user-list').classList.toggle('collapsed');
 };
