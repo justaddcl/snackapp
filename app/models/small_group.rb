ea250679795fcf4bffc54501626868ca2859f058
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-12 23:18:53 -0500

    small group night method
    added:
    - .night method that would return the night of the discipleship community that the group is assigned to

--- app/models/small_group.rb
+++ app/models/small_group.rb
@@ -5,4 +5,9 @@ class SmallGroup < ApplicationRecord
   belongs_to :leader, class_name: 'User'
   has_many :user_roles
   has_many :events, as: :gatherable
+
+  # returns group night
+  def night
+    discipleship_community.night
+  end
 end
